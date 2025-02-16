33ab3cd5f6ffc98b02406338ad77e18e215d5ef9
west_east <west.east1832@gmail.com>
2025-02-16 23:11:27 +0900

    solved: java Boj1629

---
+++ math/Boj1629.java
@@ -0,0 +1,31 @@
+package math;
+
+import java.io.*;
+import java.util.*;
+
+//백준 곱셈 Boj1629
+public class Boj1629 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+
+        System.out.println(pow(A, B, C));
+    }
+
+    static long pow(long A, long B, long C) {
+        if(B == 0) return 1;
+        if(B == 1) return A % C;
+
+        long temp = pow(A, B/2, C);
+
+        if(B % 2 == 0) {
+            return (temp * temp) % C;
+        } else {
+            return (((temp * temp) % C) * A) % C;
+        }
+    }
+}
