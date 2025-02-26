bdf7e4e33f6bb5181bc1fa23a8b149b509c0a3e0
west_east <west.east1832@gmail.com>
2025-02-26 14:51:14 +0900

    solved: java Boj11727

---
+++ dp/Boj11727.java
@@ -0,0 +1,21 @@
+package dp;
+
+import java.io.*;
+
+//백준 2xn 타일링 2
+public class Boj11727 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] d = new int[n + 1];
+        d[1] = 1;
+        d[2] = 3;
+        for (int i = 3; i <= n; i++) {
+            d[i] = (d[i - 1] + d[i - 2] * 2) % 10007;
+        }
+        System.out.println(d[n]);
+    }
+
+}
