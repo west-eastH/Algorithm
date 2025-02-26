dca492ed445fa548eb2c5ec8f3c0e6f47df93d2b
west_east <west.east1832@gmail.com>
2025-02-26 15:26:54 +0900

    solved: java Boj17626

--- dp/Boj11727.java
+++ dp/Boj11727.java
@@ -1,6 +1,7 @@
 package dp;
 
 import java.io.*;
+import java.util.*;
 
 //백준 2xn 타일링 2
 public class Boj11727 {
@@ -9,7 +10,7 @@ public class Boj11727 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] d = new int[n + 1];
+        int[] d = new int[n + 2];
         d[1] = 1;
         d[2] = 3;
         for (int i = 3; i <= n; i++) {
---
+++ dp/Boj17626.java
@@ -0,0 +1,26 @@
+package dp;
+
+import java.io.*;
+
+//백준 Four Squares Boj17626
+public class Boj17626 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] d = new int[n + 1];
+        d[0] = 0;
+        d[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            d[i] = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                d[i] = Math.min(d[i], d[i - j * j] + 1);
+            }
+        }
+        System.out.println(d[n]);
+    }
+
+}
