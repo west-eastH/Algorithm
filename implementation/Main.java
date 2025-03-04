1ac43c9a466f46d428f98fbd2ee7ce1f52ae0f29
west_east <west.east1832@gmail.com>
2025-03-04 15:13:14 +0900

    solved: java Boj6064

--- implementation/Main.java
+++ implementation/Main.java
@@ -4,41 +4,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static StringBuilder sb = new StringBuilder();
     static int n;
-    static int[] arr;
-    static int[] sorted;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-
-        arr = new int[n];
-        sorted = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
+        n = Integer.parseInt(st.nextToken());
+        long[][] d = new long[n + 1][2];
+        d[1][0] = 0;
+        d[1][1] = 1;
+        for (int i = 2; i <= n; i++) {
+            d[i][0] = d[i - 1][0] + d[i - 1][1];
+            d[i][1] = d[i - 1][0];
         }
 
-        sorted = arr.clone();
-        Arrays.sort(sorted);
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int rank = 0;
-        for (int i = 0; i < n; i++) {
-            if (!map.containsKey(sorted[i])) {
-                map.put(sorted[i], rank);
-                rank++;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            sb.append(map.get(arr[i])).append(" ");
-        }
-
-        System.out.println(sb);
+        System.out.println(d[n][0] + d[n][1]);
     }
-
-
 }
