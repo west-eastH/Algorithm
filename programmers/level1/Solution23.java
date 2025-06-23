56b0ecf329b686afb425d1779d242bf8ecd7226f
west_east <west.east1832@gmail.com>
2025-06-23 13:23:24 +0900

    solved: java programmers level1 solved 1 problems

---
+++ programmers/level1/Solution23.java
@@ -0,0 +1,28 @@
+package programmers.level1;
+
+class Solution23 {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] dp = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
+                    dp[i]++;
+                    if (j * j != i) {
+                        dp[i]++;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (dp[i] > limit) {
+                dp[i] = power;
+            }
+            answer += dp[i];
+        }
+
+        return answer;
+    }
+}
