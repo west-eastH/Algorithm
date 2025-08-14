6b836b47c4b19d3245545c41ccc5384023e13692
west_east <west.east1832@gmail.com>
2025-08-14 16:24:03 +0900

    solved: java Boj3085

---
+++ implementation/Boj3085.java
@@ -0,0 +1,78 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//백준 사탕 게임 Boj3085
+public class Boj3085 {
+    static char[][] board;
+    static int N;
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        board = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            board[i] = br.readLine().toCharArray();
+        }
+
+        checkCount();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (j + 1 < N) {
+                    swap(i, j, i, j + 1);
+                    checkCount();
+                    swap(i, j, i, j + 1);
+                }
+
+                if (i + 1 < N) {
+                    swap(i, j, i + 1, j);
+                    checkCount();
+                    swap(i, j, i + 1, j);
+                }
+            }
+        }
+        System.out.println(max);
+
+    }
+
+    public static void swap(int x1, int y1, int x2, int y2) {
+        char temp = board[x1][y1];
+        board[x1][y1] = board[x2][y2];
+        board[x2][y2] = temp;
+    }
+
+    public static void checkCount() {
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+            for (int j = 1; j < N; j++) {
+                if (board[i][j] == board[i][j - 1]) {
+                    cnt++;
+                } else {
+                    max = Math.max(max, cnt);
+                    cnt = 1;
+                }
+            }
+            max = Math.max(max, cnt);
+        }
+
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+
+            for (int j = 1; j < N; j++) {
+                if (board[j][i] == board[j - 1][i]) {
+                    cnt++;
+                } else {
+                    max = Math.max(max, cnt);
+                    cnt = 1;
+                }
+            }
+            max = Math.max(max, cnt);
+        }
+    }
+
+}
