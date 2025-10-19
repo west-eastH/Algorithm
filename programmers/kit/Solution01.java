cb2a67eee6efe0a6eb59210b6a63543e45a030e9
west_east <west.east1832@gmail.com>
2025-10-19 14:18:07 +0900

    solved: java programmers kit solved 1 problem

---
+++ programmers/kit/Solution01.java
@@ -0,0 +1,21 @@
+package programmers.kit;
+
+import java.util.*;
+
+//프로그래머스 알고리즘 고득점 Kit 의상
+class Solution01 {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
+        }
+
+        int result = 1;
+        for (int cnt : map.values()) {
+            result *= cnt;
+        }
+        result -= 1;
+        return result;
+    }
+}
