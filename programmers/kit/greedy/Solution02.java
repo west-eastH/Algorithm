6de6f7020a331e2e944fde6e73ecda165f165677
west_east <west.east1832@gmail.com>
2026-02-08 17:51:11 +0900

    solved: java programmers kit solved 1 problem

---
+++ programmers/kit/greedy/Solution02.java
@@ -0,0 +1,25 @@
+package programmers.kit.greedy;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < number.length(); i++) {
+            while (!stack.isEmpty() && number.charAt(i) > stack.peek() && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(number.charAt(i));
+        }
+        for (int i = 0; i < k; i++) {
+            stack.pop();
+        }
+        for (char c : stack) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
